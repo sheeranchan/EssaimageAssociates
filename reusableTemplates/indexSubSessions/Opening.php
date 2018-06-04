3886ce6ec69f26552ccb39e8946e49ee9af0b048
sheeranchan <sheeranchan@gmail.com>
2018-06-04 17:09:27 +0100

    background issue

--- reusableTemplates/indexSubSessions/Opening.php
+++ reusableTemplates/indexSubSessions/Opening.php
@@ -3,5 +3,5 @@
         <h1 data-toggle="tooltip" title="In France, they talk in a metaphoric sense about an [Essaimage] of new ideas, techniques, knowledge and savoir-faire (know-how, savvy, from years of experience)."> Essaimage Associates Limited</h1>
         <p class="tagline">Creates and sustains relationships which enable technology businesses to find talent, innovate, and grow market share!</p>
     </div>
-    <div class="img-holder mt-3 animated fadeInLeft delay-10s"><img src="images/WalkerCOLOUR.png" alt="Robert Owen Icon" class="img-fluid margin-top-10"></div>
+    <div class="img-holder mt-3 animated fadeInLeft delay-10s"><img src="../../images/WalkerCOLOUR.png" alt="Robert Owen Icon" class="img-fluid margin-top-10"></div>
 </header>
