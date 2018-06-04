b5f6c352f102bb69f35f724546772cc2ec65a8cb
sheeranchan <sheeranchan@gmail.com>
2018-06-04 22:57:36 +0100

    pause carousels when hover

--- reusableTemplates/navMenu.php
+++ reusableTemplates/navMenu.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-12">
             <nav class="navbar navbar-dark navbar-expand-lg">
-                <a class="navbar-brand" href="<?php echo '../index.php';?>"><img src="../images/Bee%20Icon%20RGB%20logo.png" class="img-fluid" alt="logo"></a> <button class="navbar-toggler small" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                <a class="navbar-brand" href="<?php echo 'http://wecnnct.co.uk/index.php';?>"><img src="../images/Bee%20Icon%20RGB%20logo.png" class="img-fluid" alt="logo"></a> <button class="navbar-toggler small" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
                 <div class="collapse navbar-collapse" id="navbar">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item"> <a class="nav-link active" href="<?php echo '../index.php';?>">Home <span class="sr-only">(current)</span></a> </li>
